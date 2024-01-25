6023382a2a85f6370621cfae4a07a1595b3ac6d5
Yusuf KAYA <yusuf.kaya1059@gmail.com>
2024-01-26 00:56:13 +0300

    FAQ finish

--- Frontend_Mentor/FAQAccordion/app.js
+++ Frontend_Mentor/FAQAccordion/app.js
@@ -1,9 +1,7 @@
 const content = document.querySelector('.content');
-const infoDivs = document.querySelectorAll('info');
-
 
 content.addEventListener('click', (event) => {
-
+    
     if (!event.target.closest('img')) return;
     const information = event.target.parentElement.nextSibling.nextSibling;
     const parag = information.getElementsByTagName('p')[0];
@@ -11,10 +9,13 @@ content.addEventListener('click', (event) => {
     if(parag.clientHeight){
         parag.style.height = 0;
         parag.style.opacity = 0;
+        event.target.attributes[1].nodeValue = "img/icon-plus.svg";
+
     }
     else{
         parag.style.height = parag.scrollHeight + 'px';
         parag.style.opacity = '0.7';
+        event.target.attributes[1].nodeValue = "img/icon-minus.svg";
     }
 
 });
